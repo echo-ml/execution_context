6cbf5ece5c601851913cf1a29494099686b32390
Ribulous Barnulous <ribulous.barnulous@gmail.com>
2015-07-01 04:00:27 -0400

    refactored pragma code

--- include/echo/execution_context/tbb/for.h
+++ include/echo/execution_context/tbb/for.h
@@ -3,21 +3,8 @@
 #include <echo/execution_context/concept.h>
 #include <echo/execution_context/option.h>
 #include <echo/index.h>
+#include <echo/pragma.h>
 #include <iostream>
-
-#ifdef __INTEL_COMPILER
-#define ECHO_PRAGMA_NONTEMPORAL _Pragma("vector nontemporal")
-#define ECHO_PRAGMA_SIMD _Pragma("simd")
-#define ECHO_PRAGMA_ALIGNED _Pragma("vector aligned")
-#define ECHO_PRAGMA_SUGGEST_INLINE _Pragma("inline recursive")
-#define ECHO_PRAGMA_FORCE_INLINE _Pragma("forceinline recursive")
-#else
-#define ECHO_PRAGMA_NONTEMPORAL 
-#define ECHO_PRAGMA_SIMD _Pragma("omp simd")
-#define ECHO_PRAGMA_ALIGNED
-#define ECHO_PRAGMA_SUGGEST_INLINE
-#define ECHO_PRAGMA_FORCE_INLINE
-#endif
 
 namespace echo {
 namespace execution_context {
@@ -389,9 +376,3 @@ ECHO_PRAGMA_FORCE_INLINE
 }
 }
 }
-
-#undef ECHO_PRAGMA_NONTEMPORAL
-#undef ECHO_PRAGMA_SIMD
-#undef ECHO_PRAGMA_ALIGNED
-#undef ECHO_PRAGMA_SUGGEST_INLINE
-#undef ECHO_PRAGMA_FORCE_INLINE
